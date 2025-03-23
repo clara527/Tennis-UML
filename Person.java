bf553b4eda46d3d76b389115182ff3201f3d7ae2
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:33:06 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,21 @@
-public class Person {
+public abstract class Person {
+    public String fullName;
+    protected int yearsOld;
+    private String country;
 
+    public Person(String fullName, int yearsOld) {
+        this.fullName = fullName;
+        this.yearsOld = yearsOld;
+        this.country = "Country not known";
+    }
+
+    public Person() {
+        this("Name is not known", 0);
+    }
+
+    public abstract void showDetails();
+
+    protected String getCountry() {
+        return country;
+    }
 }
