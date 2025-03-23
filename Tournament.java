5c9bdc10bb124f9844cead08e3553bf1ae1b5aae
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:33:42 -0400

    Update Tournament.java

--- Tournament.java
+++ Tournament.java
@@ -1,3 +1,15 @@
-public class Tournament {
+public abstract class Tournament {
+    public String tourneyName;
+    protected int cashPrize;
 
+    public Tournament(String tourneyName, int cashPrize) {
+        this.tourneyName = tourneyName;
+        this.cashPrize = cashPrize;
+    }
+
+    public Tournament() {
+        this("Tournament is not known", 0);
+    }
+
+    public abstract void kickOff();
 }
