270074aa2e93e2fdc909871aed59c082b9835512
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:33:23 -0400

    Update Referee.java

--- Referee.java
+++ Referee.java
@@ -1,3 +1,22 @@
-public class Referee {
+public class Referee extends Person {
+    private int yearsExperience;
 
+    public Referee(String fullName, int yearsOld, int yearsExperience) {
+        super(fullName, yearsOld);
+        this.yearsExperience = yearsExperience;
+    }
+
+    public Referee() {
+        super();
+        this.yearsExperience = 0;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println(fullName + " is a referee with " + yearsExperience + " years of coaching experience");
+    }
+
+    public void refGame() {
+        System.out.println(fullName + " is reffing the game today");
+    }
 }
