68e91cf023550a3a934a118f1a26ac564f7822ab
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:34:44 -0400

    Update TennisRankings.java

--- TennisRankings.java
+++ TennisRankings.java
@@ -1,3 +1,22 @@
-public class TennisRankings {
+import java.util.List;
+import java.util.ArrayList;
 
+public class TennisRankings {
+    private List<TennisPlayer> rankedPlayers;
+
+    public TennisRankings() {
+        rankedPlayers = new ArrayList<>();
+    }
+
+    public void refreshRankings() {
+        System.out.println("Refreshing player rankings...");
+        for (TennisPlayer player : rankedPlayers) {
+            player.checkRank();
+        }
+    }
+
+    public void addPlayer(TennisPlayer player) {
+        rankedPlayers.add(player);
+        System.out.println(player.fullName + " added to the rankings.");
+    }
 }
