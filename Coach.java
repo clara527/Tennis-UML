12d846c0b310ac42c2bfbaed1a1cf9b33aa52be3
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:31:50 -0400

    Update Coach.java

--- Coach.java
+++ Coach.java
@@ -1,3 +1,22 @@
-public class Coach {
+public class Coach extends Person {
+    private String gamePlan;
 
+    public Coach(String fullName, int yearsOld, String gamePlan) {
+        super(fullName, yearsOld);
+        this.gamePlan = gamePlan;
+    }
+
+    public Coach() {
+        super();
+        this.gamePlan = "Default Game Plan";
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println(fullName + " is a coach with the game plan: " + gamePlan);
+    }
+
+    public void trainAthlete() {
+        System.out.println(fullName + " is training the player and game plan is" + gamePlan);
+    }
 }
