1310d9a0bde0f06c5b0e8f0bf49ec864988431d7
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:32:14 -0400

    Update GrandSlamTournament.java

--- GrandSlamTournament.java
+++ GrandSlamTournament.java
@@ -1,3 +1,11 @@
-public class GrandSlamTournament {
+public class GrandSlamTournament extends Tournament {
+    public GrandSlamTournament(String tourneyName, int cashPrize) {
+        super(tourneyName, cashPrize);
+    }
 
+    @Override
+    public void kickOff() {
+        System.out.println(tourneyName + " Grand Slam is kicking off with a prize of $" + cashPrize);
+    }
 }
+
