e64e29b53d4f9ffc0fedb9aa8a62de8de4e1249b
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:34:30 -0400

    Update TennisPlayer.java

--- TennisPlayer.java
+++ TennisPlayer.java
@@ -1,3 +1,28 @@
-public class TennisPlayer {
+public class TennisPlayer extends Person implements Playable, Rankable {
+    private int rankSpot;
 
+    public TennisPlayer(String fullName, int yearsOld, int rankSpot) {
+        super(fullName, yearsOld);
+        this.rankSpot = rankSpot;
+    }
+
+    public TennisPlayer() {
+        super();
+        this.rankSpot = 0;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println(fullName + " is a tennis player and is " + yearsOld + " with rank " + rankSpot);
+    }
+
+    @Override
+    public void playGame() {
+        System.out.println(fullName + " is playing a competitive tennis match");
+    }
+
+    @Override
+    public void checkRank() {
+        System.out.println(fullName + " current ranking: " + rankSpot);
+    }
 }
