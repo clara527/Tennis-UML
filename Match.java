2bed445b53273add6fce42d3b3b5edcfa4e0614c
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:32:49 -0400

    Update Match.java

--- Match.java
+++ Match.java
@@ -1,3 +1,16 @@
 public class Match {
+    private TennisPlayer playerA;
+    private TennisPlayer playerB;
 
+    public Match(TennisPlayer playerA, TennisPlayer playerB) {
+        this.playerA = playerA;
+        this.playerB = playerB;
+    }
+
+    public void beginMatch() {
+        System.out.println("Match between " + playerA.fullName + " and " + playerB.fullName + " is starting now!");
+        playerA.playGame();
+        playerB.playGame();
+        System.out.println("The match has ended. Check the results online!");
+    }
 }
