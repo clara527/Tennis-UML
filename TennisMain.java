27326152ea46048a1967c5902a6e7121eb4fe655
clara527 <74825920+clara527@users.noreply.github.com>
2025-03-22 20:34:03 -0400

    Update TennisMain.java

--- TennisMain.java
+++ TennisMain.java
@@ -1,3 +1,19 @@
 public class TennisMain {
+    public static void main(String[] args) {
+        TennisPlayer player1 = new TennisPlayer("Roger Federer", 40, 1);
+        TennisPlayer player2 = new TennisPlayer("Rafael Nadal", 38, 2);
 
+        Match match = new Match(player1, player2);
+        match.beginMatch();
+
+        GrandSlamTournament gs = new GrandSlamTournament("US Open", 1000000);
+        Helper.printTourneyDetails(gs);
+        gs.kickOff();
+
+        Coach coach = new Coach("Ivan Lendl", 60, "Offensive Play");
+        coach.trainAthlete();
+
+        Referee ref = new Referee("Carlos Ramos", 45, 20);
+        ref.refGame();
+    }
 }
